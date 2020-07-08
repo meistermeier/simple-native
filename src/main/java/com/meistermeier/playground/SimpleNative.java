47b5a2bb712239d56a12ee39f70b2829b2f89596
Gerrit Meier <meistermeier@gmail.com>
2020-07-08 22:02:41 +0200

    Simple native-image project.

---
+++ src/main/java/com/meistermeier/playground/SimpleNative.java
@@ -0,0 +1,11 @@
+package com.meistermeier.playground;
+
+/**
+ * @author Gerrit Meier
+ */
+public class SimpleNative {
+
+	public static void main(String[] args) {
+		System.out.println("Native");
+	}
+}
